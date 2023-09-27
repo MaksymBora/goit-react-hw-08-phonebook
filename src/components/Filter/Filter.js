f2a6da80844a8318ccd12fd651da3de1a8c6c5c1
MaksymBora <maxboraod@gmail.com>
2023-09-27 14:26:42 +0200

    udpdate filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -5,9 +5,13 @@ import { SearchStyled, PaperStyledSearch } from './Filter.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { contactsFilter } from 'redux/contacts/filterSlice';
 import { selectFilteredContact } from 'redux/contacts/selectors';
+import { selectTheme } from 'redux/userTheme/slice';
 
 export const Filter = () => {
   const nameFromFilter = useSelector(selectFilteredContact);
+  const userTheme = useSelector(selectTheme);
+  const userColorIcon =
+    userTheme === 'dark' ? 'rgb(99,230,120)' : 'rgb(105, 105, 105)';
   const dispatch = useDispatch();
 
   return (
@@ -18,17 +22,21 @@ export const Filter = () => {
         p: '2px 4px',
         display: 'flex',
         alignItems: 'center',
-        bgcolor: 'rgb(241 243 244)',
+        bgcolor: 'transparent',
         boxShadow: 0,
         borderRadius: '8px',
         transition: 'background-color 250ms ease-in, box-shadow 250ms ease-in',
       }}
     >
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+      <IconButton
+        type="button"
+        sx={{ p: '10px', color: userColorIcon }}
+        aria-label="search"
+      >
         <SearchIcon />
       </IconButton>
       <SearchStyled
-        sx={{ ml: 1, flex: 1 }}
+        sx={{ ml: 1, flex: 1, color: '#c1c1c1' }}
         placeholder="Search..."
         inputProps={{ 'aria-label': 'filter' }}
         name="filter"
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -3,12 +3,14 @@ import { InputBase } from '@mui/material';
 
 export const PaperStyledSearch = styled(InputBase)`
   box-shadow: 0 0 0 transparent;
+  border: 1px solid ${props => props.theme.colors.header.border};
   transition: background-color 250ms ease-in;
   width: 100%;
 
   &:focus-within {
     background-color: transparent;
-    box-shadow: 0px 4px 8px 2px rgba(0, 0, 0, 0.52);
+    box-shadow: 0px 4px 8px 2px
+      ${props => props.theme.colors.header.boxShadowHover};
   }
 
   @media screen and (min-width: 767px) {
@@ -18,4 +20,12 @@ export const PaperStyledSearch = styled(InputBase)`
 
 export const SearchStyled = styled(InputBase)`
   background-color: inherit;
+
+  &::placeholder {
+    color: red;
+  }
+
+  &:focus-within {
+    color: ${props => props.theme.colors.header.text};
+  }
 `;
