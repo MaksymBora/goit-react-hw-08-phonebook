c9b480f362dfe084a2a30b24f28789761326ab76
MaksymBora <maxboraod@gmail.com>
2023-10-01 23:18:35 +0200

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -59,7 +59,7 @@ export const App = () => {
             <Route path="addContact" element={ <PrivateRoute component={ <AddContacts /> } redirectTo="/login" /> } />
             <Route path="login" element={ <RestrictedRoute component={ <AuthUserForm /> } redirectTo="/" /> } />
             </Route>
-            <Route path='*' element={ <RestrictedRoute component={ <NotFound /> } redirectTo="/" /> }/>
+            <Route path='*' />
         </Routes>
       </>
     ) }
